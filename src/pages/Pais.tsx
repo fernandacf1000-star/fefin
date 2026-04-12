a83ea56af04f1c3c75c446f31ba8876ed40d6661
fernandacf1000-star <fernandacf1000@gmail.com>
2026-04-12 14:49:47 -0300

    Update Pais.tsx

--- src/pages/Pais.tsx
+++ src/pages/Pais.tsx
@@ -174,12 +174,6 @@ export default function Pais() {
   const mesLabel = getMesLabel(mesAtual.year, mesAtual.month);
 
   const { data: todos = [], isLoading } = useLancamentos(mesRef);
-  const { data: todosReembolsos = [] } = useAllReembolsos();
-  const idsLancamentos = new Set(todos.map((l) => l.id));
-
-  const reembolsosValidos = todosReembolsos.filter((r) =>
-    idsLancamentos.has(r.lancamento_id)
-  );
 
   const prevMes = () =>
     setMesAtual(({ year, month }) => (month === 0 ? { year: year - 1, month: 11 } : { year, month: month - 1 }));
